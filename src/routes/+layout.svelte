90e9a30107eedc2f0fbac45d8ca5064a1c11a9b5
Hadi Khir <hadi.khir@sap.com>
2023-10-25 17:43:19 -0400

    adding header component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,19 @@
 <script>
 	import '../app.css';
+	import Header from './Header.svelte';
 </script>
 
 <div class="flex flex-col min-h-screen">
-	<header class="flex flex-col justify-center items-center p-2">
-        <nav>
-            <div class="container mx-auto">
-              <div class="flex justify-between items-center">
-                <ul class="flex space-x-4">
-                  <li><a href="/" class="text-black">Home</a></li>
-                  <li><a href="/about" class="text-black">About</a></li>
-                  <li><a href="/services" class="text-black">Services</a></li>
-                  <li><a href="/contact" class="text-black">Contact</a></li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-	</header>
 
-	<main class="flex-1 flex flex-col p-4 max-w-screen-lg mx-auto">
-		<slot />
-	</main>
+  <Header/>
 
-	<footer class="flex flex-col justify-center items-center p-2">
-		<p>Thanks for stopping by!</p>
-	</footer>
+  <main class="flex-1 flex flex-col p-4 max-w-screen-lg mx-auto">
+    <slot />
+  </main>
+
+  <footer class="flex flex-col justify-center items-center p-2">
+    <p>Thanks for stopping by!</p>
+  </footer>
 </div>
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,28 @@
-<p class="text-2xl font-bold">
-    Hey, I'm Hadi! I wanted somewhere to showcase the pictures that I take, as a digital library but elsewhere from Instagram. Thanks for stopping by!
-</p>
+<script>
+    // Your Svelte component logic here
+</script>
+
+<div class="flex">
+    <div class="grid grid-cols-2 gap-4">
+        <div class="p-4 bg-gray-200">
+            <!-- Content for the first item -->
+            <h2 class="text-xl font-bold">Item 1</h2>
+            <p>This is the content for the first item.</p>
+        </div>
+        <div class="p-4 bg-gray-200">
+            <!-- Content for the second item -->
+            <h2 class="text-xl font-bold">Item 2</h2>
+            <p>This is the content for the second item.</p>
+        </div>
+        <div class="p-4 bg-gray-200">
+            <!-- Content for the third item -->
+            <h2 class="text-xl font-bold">Item 3</h2>
+            <p>This is the content for the third item.</p>
+        </div>
+        <div class="p-4 bg-gray-200">
+            <!-- Content for the fourth item -->
+            <h2 class="text-xl font-bold">Item 4</h2>
+            <p>This is the content for the fourth item.</p>
+        </div>
+    </div>
+</div>
