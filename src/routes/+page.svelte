ea5d7f07d93293a5f42ee03a3056c15685b6fda8
Hadi Khir <hadi.khir@sap.com>
2023-10-26 16:55:50 -0400

    adding album component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,18 @@
 <script>
 	// Your Svelte component logic here
+
+	import Album from "./Album.svelte";
+
 </script>
 
 <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-12">
-	<div class="w-full h-0 shadow-lg aspect-w-1 aspect-h-1 relative">
-		<div>
-			<img
-				src={'https://czlkmhdfsockxgulkjtb.supabase.co/storage/v1/object/public/photos/Birds/european_starlings.jpg'}
-				alt={'European Starlings on windvine'}
-                class="aspect-square w-full h-full"
-			/>
-			<div class="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gray-800 opacity-70">
-                <h3 class="text-xl text-white font-bold">
-                    Birds
-                </h3>
-            </div>
-		</div>
-	</div>
-	<div class="w-full h-0 shadow-lg aspect-w-1 aspect-h-1 relative">
-		<div>
-			<img
-				src={'https://czlkmhdfsockxgulkjtb.supabase.co/storage/v1/object/public/photos/Cities/DSCF9352.jpg?t=2023-10-26T18%3A17%3A34.112Z'}
-				alt={'Lookout tower'}
-                class="aspect-square w-full h-full"
-			/>
-			<div class="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gray-800 opacity-70">
-                <h3 class="text-xl text-white font-bold">
-                    Around the world
-                </h3>
-            </div>
-		</div>
-	</div>
+	<Album 
+		altText={'European Starlings on windvine'} 
+		imgSrc={'https://czlkmhdfsockxgulkjtb.supabase.co/storage/v1/object/public/photos/Birds/european_starlings.jpg'}
+		title={'Birds'} />
+
+	<Album 
+	altText={'Lookout tower'} 
+	imgSrc={'https://czlkmhdfsockxgulkjtb.supabase.co/storage/v1/object/public/photos/Cities/DSCF9352.jpg?t=2023-10-26T18%3A17%3A34.112Z'}
+	title={'Around the world'} />
 </div>
