60eb8e8abb748f659fd361818db29993c38d5e08
Hadi Khir <hadi.khir@sap.com>
2023-10-26 14:19:31 -0400

    album preview

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,32 @@
 </script>
 
 <div class="grid grid-cols-2 gap-4 mt-12">
-    <div class="w-full h-0 shadow-lg aspect-w-1 aspect-h-1 rounded-xl bg-yellow-300"></div>
-    <div class="w-full h-0 shadow-lg aspect-w-1 aspect-h-1 rounded-xl bg-red-300"></div>
-    <div class="w-full h-0 shadow-lg aspect-w-1 aspect-h-1 rounded-xl bg-green-300"></div>
-    <div class="w-full h-0 shadow-lg aspect-w-1 aspect-h-1 rounded-xl bg-indigo-300"></div>
+	<div class="w-full h-0 shadow-lg aspect-w-1 aspect-h-1 relative">
+		<div>
+			<img
+				src={'https://czlkmhdfsockxgulkjtb.supabase.co/storage/v1/object/public/photos/common_pigeon.jpg?t=2023-10-26T18%3A09%3A49.838Z'}
+				alt={'Seagull overlooking pond'}
+                class="aspect-square w-full h-full"
+			/>
+			<div class="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gray-800 opacity-70">
+                <h3 class="text-xl text-white font-bold">
+                    Birds
+                </h3>
+            </div>
+		</div>
+	</div>
+	<div class="w-full h-0 shadow-lg aspect-w-1 aspect-h-1 relative">
+		<div>
+			<img
+				src={'https://czlkmhdfsockxgulkjtb.supabase.co/storage/v1/object/public/photos/Cities/DSCF9352.jpg?t=2023-10-26T18%3A17%3A34.112Z'}
+				alt={'Lookout tower'}
+                class="aspect-square w-full h-full"
+			/>
+			<div class="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gray-800 opacity-70">
+                <h3 class="text-xl text-white font-bold">
+                    Around the world
+                </h3>
+            </div>
+		</div>
+	</div>
 </div>
