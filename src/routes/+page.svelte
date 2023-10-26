900b0ef9f6865e3e703326c02f334a48c60ca8a4
Hadi Khir <hadi.khir@sap.com>
2023-10-26 16:58:45 -0400

    file cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,17 @@
 <script>
-	// Your Svelte component logic here
-
-	import Album from "./Album.svelte";
-
+	import Album from './Album.svelte';
 </script>
 
 <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-12">
-	<Album 
-		altText={'European Starlings on windvine'} 
+	<Album
+		altText={'European Starlings on windvine'}
 		imgSrc={'https://czlkmhdfsockxgulkjtb.supabase.co/storage/v1/object/public/photos/Birds/european_starlings.jpg'}
-		title={'Birds'} />
+		title={'Birds'}
+	/>
 
-	<Album 
-	altText={'Lookout tower'} 
-	imgSrc={'https://czlkmhdfsockxgulkjtb.supabase.co/storage/v1/object/public/photos/Cities/DSCF9352.jpg?t=2023-10-26T18%3A17%3A34.112Z'}
-	title={'Around the world'} />
+	<Album
+		altText={'Lookout tower'}
+		imgSrc={'https://czlkmhdfsockxgulkjtb.supabase.co/storage/v1/object/public/photos/Cities/DSCF9352.jpg?t=2023-10-26T18%3A17%3A34.112Z'}
+		title={'Around the world'}
+	/>
 </div>
