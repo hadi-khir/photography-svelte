b26a6916bc4e80dfacd0cf0115c55b5fb11f7461
Hadi Khir <hadi.khir@sap.com>
2023-10-27 13:58:12 -0400

    updating album css for hover

--- src/routes/Album.svelte
+++ src/routes/Album.svelte
@@ -6,15 +6,15 @@
 
 </script>
 
-<div class="w-full h-0 shadow-lg aspect-w-1 aspect-h-1 relative">
-    <div>
+<div class="w-full h-0 shadow hover:shadow-2xl aspect-w-1 aspect-h-1 relative hover:cursor-pointer">
+    <div class="group">
         <img
             src={imgSrc}
             alt={altText}
-            class="aspect-square w-full h-full"
+            class="aspect-square w-full h-full group-hover:opacity-95"
         />
-        <div class="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gray-800 opacity-70">
-            <h3 class="text-xl text-white font-bold">
+        <div class="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gray-800 opacity-70 group-hover:opacity-80">
+            <h3 class="text-xl text-white font-bold group-hover:text-2xl">
                 {title}
             </h3>
         </div>
