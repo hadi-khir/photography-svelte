e12d56d679d7a0dfb5d14adf26855bb4f167040d
Hadi Khir <hadi.khir@sap.com>
2023-10-31 17:44:04 -0400

    adding loader for images

--- src/routes/albums/[album]/GalleryImage.svelte
+++ src/routes/albums/[album]/GalleryImage.svelte
@@ -1,24 +1,57 @@
 <script lang="ts">
-    
-	import type { IfcPhoto } from '../../../interfaces/IfcPhoto';
-	
-	export let photo: IfcPhoto;
-	
-	const getPhotoInfo = (photo: IfcPhoto) => {
+	import { onMount } from 'svelte';
+	import ContentLoader from 'svelte-content-loader';
+
+	export let photo: IPhoto;
+
+	let loaded = false;
+	let failed = false;
+	let loading = false;
+
+	onMount(() => {
+		const img = new Image();
+		img.src = photo.url;
+		loading = true;
+		img.onload = () => {
+			loaded = true;
+			loading = false;
+		};
+		img.onerror = () => {
+			failed = true;
+			loading = false;
+		};
+	});
+
+	const getPhotoInfo = (photo: IPhoto) => {
 		return {
 			imgSrc: photo.thumbnail,
 			altText: photo.title,
-			id: photo.id,
+			id: photo.id
 		};
-	}
+	};
 </script>
 
-<div>
-	<a href="/photos/{getPhotoInfo(photo).id}">
-		<img
-			class="hover:scale-110 transition duration-500 cursor-pointer h-64 w-96"
-			src={getPhotoInfo(photo).imgSrc}
-			alt={getPhotoInfo(photo).altText}
-		/>
-	</a>
-</div>
+{#if loaded}
+	<div>
+		<a href="/photos/{getPhotoInfo(photo).id}">
+			<img
+				class="hover:scale-110 transition duration-500 cursor-pointer h-64 w-96"
+				src={getPhotoInfo(photo).imgSrc}
+				alt={getPhotoInfo(photo).altText}
+			/>
+		</a>
+	</div>
+{:else if failed}
+	<div>
+		<p class="text-white">Failed to load image</p>
+	</div>
+{:else if loading}
+	<ContentLoader>
+		<rect x="0" y="0" rx="3" ry="3" width="250" height="10" />
+		<rect x="20" y="20" rx="3" ry="3" width="220" height="10" />
+		<rect x="20" y="40" rx="3" ry="3" width="170" height="10" />
+		<rect x="0" y="60" rx="3" ry="3" width="250" height="10" />
+		<rect x="20" y="80" rx="3" ry="3" width="200" height="10" />
+		<rect x="20" y="100" rx="3" ry="3" width="80" height="10" />
+	</ContentLoader>
+{/if}
