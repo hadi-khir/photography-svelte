a47815daedb5d25f6c69c18755f1b9d69040e8ea
Hadi Khir <hadi.khir@sap.com>
2023-11-01 16:55:44 -0400

    adding analytics tag

---
+++ src/lib/Analytics.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { page } from '$app/stores'
+  
+    $: {
+      if (typeof gtag !== 'undefined') {
+        gtag('config', 'G-T9HD9RZ728', {
+          page_title: document.title,
+          page_path: $page.url.pathname,
+        })
+      }
+    }
+  </script>
+  
+  <svelte:head>
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-T9HD9RZ728">
+    </script>
+    <script>
+      window.dataLayer = window.dataLayer || []
+  
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+  
+      gtag('js', new Date())
+      gtag('config', 'G-T9HD9RZ728')
+    </script>
+  </svelte:head>
+  
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,19 @@
 <script>
+	import Analytics from '$lib/Analytics.svelte';
 	import '../app.css';
 	import Header from './Header.svelte';
 </script>
 
 <div class="flex flex-col min-h-screen bg-gray-900">
-  <Header/>
+	<Header />
 
-  <main class="flex-1 container mx-auto p-4">
-    <slot />
-  </main>
+	<main class="flex-1 container mx-auto p-4">
+		<slot />
+	</main>
 
-  <footer class="text-center py-4 text-white">
-    <p>Thanks for stopping by!</p>
-  </footer>
+	<footer class="text-center py-4 text-white">
+		<p>Thanks for stopping by!</p>
+	</footer>
+
+	<Analytics />
 </div>
-
-
